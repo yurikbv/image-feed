82fd353f46c605f0cfaee7beca660b35f3f74068
yurikbv <yurikbvv@gmail.com>
2021-03-29 23:18:07 +0300

    first commit

---
+++ components/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View, ColorPropType, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+const MyComponent = () => {
+  return (
+    <Text>
+    
+    </Text>
+  );
+};
+
+export default MyComponent;
